b200874d71584efdf5d1ad8d0a078c24ee806a9b
WonseokChoi <wonseok0304@gmail.com>
2023-06-22 10:07:47 +0900

    프로젝트생성

---
+++ src/test/java/com/sg/leo/Zblog2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.leo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zblog2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
